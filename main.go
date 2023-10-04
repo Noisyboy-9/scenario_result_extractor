d381f57d2fc0f8a0cb99a38a89fd7281e198e475
sina shariati <sina.shariati@yahoo.com>
2023-10-05 00:13:42 +0330

    Add gracefull shutdown to application

--- main.go
+++ main.go
@@ -1,6 +1,10 @@
 package main
 
 import (
+	"os"
+	"os/signal"
+	"syscall"
+
 	"github.com/noisyboy-9/golang_api_template/internal/config"
 	"github.com/noisyboy-9/golang_api_template/internal/log"
 	"github.com/noisyboy-9/golang_api_template/internal/service"
@@ -11,4 +15,9 @@ func main() {
 	config.Init()
 	log.Init()
 	service.Init()
+
+	done := make(chan os.Signal, 1)
+	signal.Notify(done, syscall.SIGINT, syscall.SIGTERM)
+	<-done
+	log.App.Info("Exit signal has been received. Shutting down . . .")
 }
