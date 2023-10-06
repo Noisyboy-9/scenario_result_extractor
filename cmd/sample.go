39a1c01947339824717c21f156e6159a013be237
sina shariati <sina.shariati@yahoo.com>
2023-10-06 13:57:37 +0330

    Add sample.go command

---
+++ cmd/sample.go
@@ -0,0 +1,30 @@
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+// sampleCmd represents the sample command
+var sampleCmd = &cobra.Command{
+	Use: "sample",
+	Run: runSampleCmd,
+}
+
+func init() {
+	rootCmd.AddCommand(sampleCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// sampleCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// sampleCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
+func runSampleCmd(cmd *cobra.Command, args []string) {
+	fmt.Println("sample called")
+}
