8664a90b9c159fcc4123fd5da1e6578142234ea2
sina shariati <sina.shariati@yahoo.com>
2023-12-29 15:06:29 +0330

    Add GetPodStatus() query

--- internal/query/pod.go
+++ internal/query/pod.go
@@ -7,43 +7,46 @@ import (
 
 	"github.com/noisyboy-9/data_extractor/internal/config"
 	"github.com/noisyboy-9/data_extractor/internal/log"
-	"github.com/noisyboy-9/data_extractor/internal/model"
 	"github.com/noisyboy-9/data_extractor/internal/service"
 	v1 "github.com/prometheus/client_golang/api/prometheus/v1"
 	promModel "github.com/prometheus/common/model"
 )
 
-func GetHpaStatus(start time.Time, end time.Time, namespace string) map[time.Time][]model.Hpa {
+func GetPodStatus(start time.Time, end time.Time, namespace string) map[time.Time]map[string][]string {
 	ctx, cancel := context.WithTimeout(context.Background(), config.Prometheus.Timeout)
 	defer cancel()
 
+	query := fmt.Sprintf("kube_pod_info{namespace='%s'}", namespace)
 	r := v1.Range{
 		Start: start,
 		End:   end,
 		Step:  config.Prometheus.Step,
 	}
-
-	query := fmt.Sprintf("kube_horizontalpodautoscaler_status_target_metric{namespace='%s'}", namespace)
 	results, warnings, err := service.Promtheus.Api.QueryRange(ctx, query, r)
 
 	if err != nil {
-		log.App.WithError(err).Panic("error in getting hpa status form prometheus")
+		log.App.WithError(err).Panic("error in getting pod status form prometheus")
 	}
 
 	if len(warnings) > 0 {
-		log.App.Warnf("hpa list warnings: %v", warnings)
+		log.App.Warnf("get pod status warnings: %v", warnings)
 	}
 
-	result := make(map[time.Time][]model.Hpa)
+	result := make(map[time.Time]map[string][]string)
 	samples := results.(promModel.Matrix)
+
 	for _, sample := range samples {
 		for _, value := range sample.Values {
-			hpas := result[value.Timestamp.Time()]
-			hpas = append(hpas, model.Hpa{
-				MetricValue: float64(value.Value),
-				Name:        string(sample.Metric["horizontalpodautoscaler"]),
-			})
-			result[value.Timestamp.Time()] = hpas
+			podPlacementStatus := result[value.Timestamp.Time()]
+			if podPlacementStatus == nil {
+				podPlacementStatus = make(map[string][]string)
+			}
+
+			pods := podPlacementStatus[string(sample.Metric["node"])]
+			pods = append(pods, string(sample.Metric["pod"]))
+			podPlacementStatus[string(sample.Metric["node"])] = pods
+
+			result[value.Timestamp.Time()] = podPlacementStatus
 		}
 	}
 
